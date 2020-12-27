5f03952a1ab4a4ef8cf721882fef935dd4925ebc
VaishaalKrishnaVS <72886020+VaishaalKrishnaVS@users.noreply.github.com>
2020-12-27 07:38:54 -0800

    added formater

--- python/hacker rank/dimensionsofcuboid.py
+++ python/hacker rank/dimensionsofcuboid.py
@@ -3,12 +3,12 @@ y = int(input())
 z = int(input())
 n = int(input())
 
-list = []    
+list = []
 
-for no in range(0,x+1):
-    for nos in range(0,y+1):
-        for ni in range(0,z+1):
-            if (no + nos +ni) != n:
-                list.append([no,nos,ni])
+for no in range(0, x+1):
+    for nos in range(0, y+1):
+        for ni in range(0, z+1):
+            if (no + nos + ni) != n:
+                list.append([no, nos, ni])
 
 print(list)
